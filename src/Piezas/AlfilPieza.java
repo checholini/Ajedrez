f00fea47512bb5b739ffb376704dd5d635c561fa
checholini <sejrl3@gmail.com>
2019-05-13 05:40:12 -0500

    Optimizacion codigo alfil

--- src/Piezas/AlfilPieza.java
+++ src/Piezas/AlfilPieza.java
@@ -35,138 +35,54 @@ public class AlfilPieza extends Pieza{
         Pieza pieza = tablero.obtenerPieza(x, y);
         switch (sc.nextInt()) {
             case 1:
-                hacerRecorrido(x, y, tablero, 1, -1);
+                hacerRecorrido(x, y, tablero, 1, -1, 1);
                 break;
             case 2:
-                hacerRecorrido(x, y, tablero, 1, 1);
+                hacerRecorrido(x, y, tablero, 1, 1,2);
                 break;
             case 3:
-                hacerRecorrido(x, y, tablero, -1, -1);
+                hacerRecorrido(x, y, tablero, -1, -1,3);
                 break;
             case 4:
 
-                hacerRecorrido(x, y, tablero, -1, 1);
+                hacerRecorrido(x, y, tablero, -1, 1,4);
                 break;
         }
         return tablero;
     }
     
     //Optimizar Luego
-    private Tablero hacerRecorrido(int x, int y, Tablero tablero, int dirY, int dirX) {
+    private Tablero hacerRecorrido(int x, int y, Tablero tablero, int dirY, int dirX, int caso) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese el numero de casillas que quiere recorrer");
         int max = Integer.parseInt(sc.nextLine());
         boolean flag = true;
-        if (dirY > 0) {
-            if (dirX > 0) {
-                if (validarRango(max, x, y, 2)) {
-                    for (int i = 1; i < max; i ++) {
-                        Pieza pieza = tablero.obtenerPieza(x + i, y + i);
-                        if (pieza != null && pieza.getJugador() != this.getJugador()) {
-                            tablero.agregarPieza(x + i, y + i, this);
-                            tablero.eliminarPieza(x, y);
-                            System.out.println("El alfil ha comido una pieza");
-                            flag = false;
-                            break;
-                        } else if (pieza != null && pieza.getJugador() == this.getJugador()) {
-                            System.out.println("El alifil no puede pasar sobre una pieza del mismo equipo");
-                            flag = false;
-                            break;
-                        }
-                    }
-                    if (flag) {
-                        tablero.agregarPieza(x + max, y + max, this);
+        if (validarRango(max, x, y, caso)) {
+                for (int i = 1; i < max; i ++) {
+                    int recorridoX = i * dirX;
+                    int recorridoY = i * dirY;
+                    Pieza pieza = tablero.obtenerPieza(x + recorridoX, y + recorridoY);
+                    if (pieza != null && pieza.getJugador() != this.getJugador()) {
+                        tablero.agregarPieza(x +recorridoX, y + recorridoY, this);
                         tablero.eliminarPieza(x, y);
+                        System.out.println("El alfil ha comido una pieza");
+                        flag = false;
+                        break;
+                    } else if (pieza != null && pieza.getJugador() == this.getJugador()) {
+                        System.out.println("El alifil no puede pasar sobre una pieza del mismo equipo");
+                        flag = false;
+                        break;
                     }
-                } else {
-                    System.out.println("El alfil no puede recorrer esa cantidad de espacios porque se saldria del tablero");
-                    System.out.println("Intentelo de nuevo");
-                    return hacerRecorrido(x, y, tablero, dirY, dirX);
+                }
+                if (flag) {
+                    tablero.agregarPieza(x + (max*dirX), y + (max*dirY), this);
+                    tablero.eliminarPieza(x, y);
                 }
             } else {
-                if (validarRango(max, x, y, 1)) {
-                    for (int i = 1; i <  max; i++) {
-                        System.out.println("pos "+(x-i) + " , "+(y+i));
-                            Pieza pieza = tablero.obtenerPieza(x-i,y+i);                            
-                            if (pieza != null && pieza.getJugador() != this.getJugador()) {
-                                tablero.agregarPieza(x-i, y+i, this);
-                                tablero.eliminarPieza(x, y);
-                                System.out.println("El alfil ha comido una pieza");
-                                flag = false;
-                                break;
-                            } else if (pieza != null && pieza.getJugador() == this.getJugador()) {
-                                System.out.println(pieza.getCaracterPieza()+" en la pos "+(x-i)+" , "+(y+1));
-                                System.out.println("El alifil no puede pasar sobre una pieza del mismo equipo");
-                                flag = false;
-                                break;
-                            }
-                    }
-                    if (flag) {
-                        tablero.eliminarPieza(x, y);
-                        tablero.agregarPieza(x - max, y + max, this);
-                    }
-                } else {
-                    System.out.println("El alfil no puede recorrer esa cantidad de espacios porque se saldria del tablero");
-                    System.out.println("Intentelo de nuevo");
-                    return hacerRecorrido(x, y, tablero, dirY, dirX);
-                }
+                System.out.println("El alfil no puede recorrer esa cantidad de espacios porque se saldria del tablero");
+                System.out.println("Intentelo de nuevo");
+                return hacerRecorrido(x, y, tablero, dirY, dirX, caso);
             }
-
-        } else {
-            if (dirX < 0) {
-                if (validarRango(max, x, y, 3)) {
-                    for (int i = 1; i < max; i ++) {
-                        Pieza pieza = tablero.obtenerPieza(x - i, y - i);
-                        if (pieza != null && pieza.getJugador() != this.getJugador()) {
-                            tablero.agregarPieza(x - i, y - i, this);
-                            tablero.eliminarPieza(x, y);
-                            System.out.println("El alfil ha comido una pieza");
-                            flag = false;
-                            break;
-                        } else if (pieza != null && pieza.getJugador() == this.getJugador()) {
-                            System.out.println("El alifil no puede pasar sobre una pieza del mismo equipo");
-                            flag = false;
-                            break;
-                        }
-                    }
-                    if (flag) {
-                        tablero.agregarPieza(x - max, y - max, this);
-                        tablero.eliminarPieza(x, y);
-                    }
-                } else {
-                    System.out.println("El alfil no puede recorrer esa cantidad de espacios porque se saldria del tablero");
-                    System.out.println("Intentelo de nuevo");
-                    return hacerRecorrido(x, y, tablero, dirY, dirX);
-                }
-            } else {
-                if (validarRango(max, x, y, 4)) {
-                    for (int i = 1; i < max; i += dirX) {
-                            Pieza pieza = tablero.obtenerPieza(x + i, y-i);
-                            if (pieza != null && pieza.getJugador() != this.getJugador()) {
-                                tablero.agregarPieza(x + i, y-i, this);
-                                tablero.eliminarPieza(x, y);
-                                System.out.println("El alfil ha comido una pieza");
-                                flag = false;
-                                break;
-                            } else if (pieza != null && pieza.getJugador() == this.getJugador()) {
-                                System.out.println("El alifil no puede pasar sobre una pieza del mismo equipo");
-                                flag = false;
-                                break;
-                            }
-                        
-                    }
-                    if (flag) {
-                        tablero.agregarPieza(x + max, y - max, this);
-                        tablero.eliminarPieza(x, y);
-                    }
-                } else {
-                    System.out.println("El alfil no puede recorrer esa cantidad de espacios porque se saldria del tablero");
-                    System.out.println("Intentelo de nuevo");
-                    return hacerRecorrido(x, y, tablero, dirY, dirX);
-                }
-            }
-
-        }
         return tablero;
     }
 
